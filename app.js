0b905ddebcabc3a28aedbcfdca33665f57aabeb2
shurikperunov@mail.ru <shurikperunov@mail.ru>
2022-11-09 19:00:26 +0500

    feat: подключиться к БД

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // import { Express } from 'express';
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
